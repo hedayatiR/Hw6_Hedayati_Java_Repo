ace1aada809ca591577b295fafe9282f6e26b34b
hedayatiR <gunner_1369@yahoo.com>
2019-04-16 13:02:48 +0430

    Adds MyLinkedList class beside internal Node Class. Methods adding element in list are written.

---
+++ Ex2_LinkedList/src/MyLinkedList.java
@@ -0,0 +1,55 @@
+
+public class MyLinkedList {
+    private Node first;
+    private Node last;
+    private int size = 0;
+    // ----------------------------------------------------------
+    // Adds element to end of the linkedList
+    public void add(Integer element) {
+        Node newNode = new Node(element);
+        if (size == 0) {
+            first = newNode;
+            last = newNode;
+        } else {
+            last.next = newNode;
+            last = newNode;
+        }
+        size++;
+    }
+    // ----------------------------------------------------------
+    // Adds element to index "index" of the linkedList
+    // if index=0 then Adds to first of list
+    public void add(int index, Integer element) {
+
+        if (index < 0 || index >= size) {
+            System.out.println("Error: index is out of bound!");
+            return;
+        }
+        // Adds to end of list
+        else if (index == (size - 1)) {
+            add(element);
+        }
+
+        else {
+            Node newNode = new Node(element);
+            // adds to first of list
+            if (index == 0) {
+                newNode.next = first;
+                first = newNode;
+            }
+            // adds in middle of list
+            else {
+                Node currentNode = first;
+                // forward index-1 steps
+                for (int i = 0; i < (index - 1); i++) {
+                    currentNode = currentNode.next;
+                }
+                newNode.next = currentNode.next;
+                currentNode.next = newNode;
+            }
+            size++;
+        }
+    }
+    // ----------------------------------------------------------
+
+}
---
+++ Ex2_LinkedList/src/Node.java
@@ -0,0 +1,8 @@
+public class Node {
+    Node next;
+    Integer data;
+
+    public Node(Integer element){
+        this.data = element;
+    }
+}
